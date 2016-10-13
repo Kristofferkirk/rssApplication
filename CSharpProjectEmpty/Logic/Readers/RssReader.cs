2c521c0f218caf112beeb1cde543129ae2fc7f42
LundkvistViktor <lundkvist.viktor@gmail.com>
2016-10-13 14:47:02 +0200

    test

---
+++ CSharpProjectEmpty/Logic/Readers/RssReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Logic.Entities;
+//Klass för avläsning av css flöde
+namespace Logic.Readers
+{
+    internal class RssReader : IReader
+    {
+        public IEnumerable<FeedItem> Read(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
