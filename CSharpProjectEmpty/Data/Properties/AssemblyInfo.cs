ba95653a682b6f70276a050d3f9c8a3d3544ad8a
Kristofferkirk <kristoffer_kg@hotmail.se>
2016-10-13 14:28:28 +0200

    Added startup files

--- CSharpProjectEmpty/Data/Properties/AssemblyInfo.cs
+++ CSharpProjectEmpty/Data/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication1")]
+[assembly: AssemblyTitle("Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication1")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Data")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0634dc86-0a8c-48c9-be05-98f73b39204c")]
+[assembly: Guid("f734ca60-6991-46d3-a3fa-a2a81528f371")]
 
 // Version information for an assembly consists of the following four values:
 //
